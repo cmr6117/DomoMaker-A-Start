9ef0d25b4195314f3f8a44aa35d96f9b7b23801c
cmr6117 <cmr6117@g.rit.edu>
2019-10-28 13:24:00 -0400

    A-16

---
+++ server/controllers/Account.js
@@ -0,0 +1,29 @@
+const models = require('../models');
+
+const Account = models.Account;
+
+const loginPage = (req, res) => {
+    res.render('login');
+};
+
+const signupPage = (req, res) => {
+    res.render('signup');
+};
+
+const logout = (req, res) => {
+    res.render('/');
+};
+
+const logout = (req, res) => {
+    
+};
+
+const logout = (req, res) => {
+    
+};
+
+module.exports.loginPage = loginPage;
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.signupPage = signupPage;
+module.exports.signup = signup;
