b87714b735cc292de9f93f78ce2bee1076cb55d1
Christian <cmr6117@gmail.com>
2019-11-01 00:40:49 -0400

    B-19

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -2,8 +2,15 @@ const models = require('../models');
 const Domo = models.Domo;
 
 const makerPage = (req, res) => {
-    res.render('app');
-}
+    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ error: 'An error occurred' });
+        }
+        
+        return res.render('app', { domos: docs });
+    });
+};
 
 const makeDomo = (req, res) => {
     if (!req.body.name || !req.body.age){
